38bd9166d95e11dd667367f44260273588c08c86
Yuhao Jin <jin1349595233@gmail.com>
2020-08-04 22:08:55 +0800

    rc1.0

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp .zshrc ~/
-cp .tmux.conf ~/
+# cp .tmux.conf ~/
 cp .p10k.zsh ~/
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
